7af95d9b71c2f3ab1b89307c896d2e07867f67d6
psychobunny <rodrigues.andrew@gmail.com>
2017-08-25 14:57:34 -0400

    move admin js into it's own file

---
+++ static/lib/admin.js
@@ -0,0 +1,36 @@
+"use strict";
+
+define('admin/plugins/custom-pages', [], function() {
+	function addCloseHandler() {
+        $('#custom-pages .fa-times').on('click', function() {
+            $(this).parents('.well').remove();
+        });
+    }
+
+    $('#add').on('click', function(ev) {
+        var clone = $('.template').clone().removeClass('template hidden');
+        $('#custom-pages').append(clone);
+
+        addCloseHandler();
+    });
+
+    addCloseHandler();
+
+    $('#save').on('click', function(ev) {
+        var arr = [];
+        $('#custom-pages .well form').each(function() {
+            var data = $(this).serializeArray();
+            if (data[1].value && !data[1].value.match(' ') && data[1].value !== '') {
+                arr.push({
+                    name: data[0].value,
+                    route: data[1].value,
+                    class: data[2].value
+                });
+			} 
+        });
+
+        socket.emit('admin.settings.saveCustomPages', arr, function() {
+            app.alertSuccess('Custom pages saved and activated');
+        });
+    });
+});
